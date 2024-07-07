1af670b210df6f6e65a10f5753571ea0159e3117
Anezz12 <harsenaargrtya1@gmail.com>
2024-07-07 22:11:34 +0700

    update

---
+++ src/app/componenets/Creations.jsx
@@ -0,0 +1,18 @@
+export default function Creations() {
+  return (
+    <section>
+      {/* container */}
+      <div className="container max-w-6xl mx-auto mt-32 px-6 text-gray-900 md:px-0">
+        {/* header */}
+        <div className=" flex justify-center mb-20 md:justify-between">
+          <h2 className="text-4xl text-center uppercase md:text-left md:text-5xl">
+            Our Creations
+          </h2>
+          <button className="hidden btn md:block">See All</button>
+        </div>
+
+        {/* item container */}
+      </div>
+    </section>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Feature from "./componenets/Feature";
 import Header from "./componenets/Header";
+import Creations from "./componenets/Creations";
 export default function Home() {
   return (
     <main>
       <Header />
       <Feature />
+      <Creations />
     </main>
   );
 }
